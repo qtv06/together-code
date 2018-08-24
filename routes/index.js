3a3bccdc24f4f0c2c1ef8bb3da318d722a549bb8
quangvovan <vanquang451@gmail.com>
2018-08-25 01:23:32 +0700

    Task2: Make a static pages with nodejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TogetherCode' });
+});
+
+router.get('/about', function(req, res) {
+  res.render('about', { title: "TogetherCode" });
 });
 
 module.exports = router;
