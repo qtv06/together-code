425c4453fd462749c8a8ec0d5ba1d8a2abb4e573
quangvovan <vanquang451@gmail.com>
2018-08-26 11:42:07 +0700

    Task3: make a contact page and send very simple email

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -16,7 +18,9 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,52 @@
 var express = require('express');
-var router = express.Router();
+var nodeMailer = require('nodemailer');
+var config = require('../config');
 
+var transporter = nodeMailer.createTransport(config.mailer);
+
+var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'TogetherCode' });
 });
 
-router.get('/about', function(req, res) {
+router.get('/about', function(req, res, next) {
   res.render('about', { title: "TogetherCode" });
 });
+
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: "TogetherCode" });
+  })
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is invalid').isEmail();
+    req.checkBody('message', 'Messages is required').notEmpty();
+    var body = req.body;
+    var errors = req.validationErrors();
+    if(errors){
+      res.render('contact', {
+        title: "TogetherCode",
+        name: body.name,
+        email: body.email,
+        message: body.message,
+        errors_mes: errors
+      });
+    }else{
+      var mailerOptions = {
+        to: body.email,
+        from: 'quangvv7500@gmail.com',
+        subject: 'You have got a messages to visitor',
+        text: body.message
+      }
+
+      transporter.sendMail(mailerOptions, function(error, info) {
+        if(error){
+          return console.log(error);
+        }
+        res.render('thank', { title: "TogetherCode" });
+      });
+    }
+  });
 
 module.exports = router;
